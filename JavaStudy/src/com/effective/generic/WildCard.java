155e929f38fdb60ea7f5806e4eb00d6d5fe96eeb
myhs2002 <myhs2002@gmail.com>
2020-01-13 23:23:19 +0900

    wildcard study

--- JavaStudy/src/com/effective/generic/WildCard.java
+++ JavaStudy/src/com/effective/generic/WildCard.java
@@ -1,9 +1,22 @@
 package com.effective.generic;
 
-class A {}
+class A {
+    public int a = 1;
+    public <T extends String> void printA(T t) {
+        System.out.println(t + " " + a);
+    }
+}
 class B extends A {}
 class C extends B {}
-class Test<T> {}
+class Test<T extends A> {
+    private T t;
+    public Test(T t) {
+        this.t = t;
+    }
+    public T print() {
+        return t;
+    }
+}
 
 public class WildCard {
     public static void wildCardTest(Test<? extends B> test) {
@@ -14,11 +27,18 @@ public class WildCard {
         System.out.println(test.getClass());
     }
 
+    public static <T extends B> void wild(Test<T> test){
+
+    }
+
     public static void main(String [] args) {
-        Test<C> test = new Test<>();
+        Test<C> test = new Test<>(new C());
+        test.print().printA("A의 결과는");
         wildCardTest(test);
 
-        Test<A> test2 = new Test<>();
+        Test<A> test2 = new Test<>(new A());
         wildCardTest2(test2);
+
+        wild(test);
     }
 }
