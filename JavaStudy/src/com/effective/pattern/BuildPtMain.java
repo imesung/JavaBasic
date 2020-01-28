39ab8a8319a98aa9508bdb84f681819c1b4aa7a8
mesung <mesung@mesungui-MacBookPro.local>
2020-01-28 22:34:19 +0900

    builder pattern study

---
+++ JavaStudy/src/com/effective/pattern/BuildPtMain.java
@@ -0,0 +1,22 @@
+package com.effective.pattern;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class BuildPtMain {
+    public static void main (String [] args) {
+        BuildPtPet.Builder builder = new BuildPtPet.Builder();
+
+        BuildPtPet pet = builder
+                .withPetName("cat")
+                .withOwnerName("mesung")
+                //.withTelephone("112")
+                .withAddress("suwon")
+                .withDateOfBirth(Calendar.getInstance().getTime())
+                .withEmailAddress("myhs2002")
+                .build();
+
+        System.out.println(pet.toString());
+
+    }
+}
