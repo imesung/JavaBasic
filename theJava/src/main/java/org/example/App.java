ef9d9580d53fd5621928344000f9abcf9435eedb
mesung <myhs2002@gmail.com>
2020-03-24 01:50:57 +0900

    접근 제한자 상관없이 모든 필드 값 가져오기(성공)

--- theJava/src/main/java/org/example/App.java
+++ theJava/src/main/java/org/example/App.java
@@ -13,6 +13,9 @@ public class App {
         Book book = new Book();
         Arrays.stream(bookClass.getDeclaredFields()).forEach(f -> {
             try {
+                //모든 접근제한자 접근
+                f.setAccessible(true);
+
                 System.out.printf("%s %s\n", f, f.get(book));
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
