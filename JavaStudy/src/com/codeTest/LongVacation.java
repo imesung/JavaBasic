2883b8fbdb993b0cc41fb003d78b5b516d79bc42
mesung <myhs2002@gmail.com>
2020-09-09 21:33:40 +0900

    codeTest update

---
+++ JavaStudy/src/com/codeTest/LongVacation.java
@@ -0,0 +1,6 @@
+package com.codeTest;
+
+//주어진 달력에서 가장 휴가를 길게 연달아 갈 수 있는 휴가일을 출력하라.
+
+public class LongVacation {
+}
---
+++ JavaStudy/src/com/codeTest/NumberAlign.java
@@ -0,0 +1,6 @@
+package com.codeTest;
+
+//주어진 숫잘들을 큰 숫자 + 작은 숫자 + 2번째 큰 숫자 + 2번째 작은 숫자 순으로 정렬하세요.
+//Ex. 35, 2, 4, 0, 22 => 35, 0, 22, 4, 0
+public class NumberAlign {
+}
