60997930655fb5f77be5b828c893a4f51edc9a11
mesung <myhs2002@gmail.com>
2020-09-10 23:56:06 +0900

    Update MaxAlphabet.java

--- JavaStudy/src/com/codeTest/MaxAlphabet.java
+++ JavaStudy/src/com/codeTest/MaxAlphabet.java
@@ -1,7 +1,7 @@
 package com.codeTest;
 
 //주어진 문자열 중 가장 많이 나온 알파벳을 출력하세요. 단 같은 횟수일 경우 빠른 알파벳을 출력하세요.
-//aabcde => a, abbccddd => d, abbccdd => b
+//aabcde => a, abbccddd => d, abbccdd => b, abcabcd => a
 public class MaxAlphabet {
     public static void main(String[] args) {
 
